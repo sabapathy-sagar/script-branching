3b45ba64f204f2dd871dc48554159eabb097d5eb
sagar rathna sabapathy <sagar.rathnasabapathy@diconium.com>
2019-07-07 08:05:18 +0200

    setup-branching-script

---
+++ branching.js
@@ -0,0 +1,25 @@
+const shell = require('shelljs');
+const readLineSync = require('readline-sync'); 
+const path = require('path');
+const { repository } = require('./branching-config.json');
+
+const { delivery, baseBranch } = repository
+const repoName = delivery.substring(delivery.lastIndexOf('/'))
+
+// Checkout to base branch
+shell.exec(`git checkout ${baseBranch}`)
+
+// Making sure we have the latest changes from the remote origin
+shell.exec(`git pull origin ${baseBranch}`)
+
+// Prompt the user of ticketID
+const ticketId = readLineSync.question('What is the Ticket ID? ',
+{
+    limit: input => input.trim().length > 0,
+    limitMessage: 'Please enter a ticket ID (e.g. DCPPD-123)'
+  })
+
+console.log('====', ticketId);
+
+// Create a new branch
+shell.exec(`git checkout -b ${ticketId}`)
